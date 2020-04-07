104ce4f17c610befb9d07b601b9cf1145b936ffd
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-04-07 17:35:40 +0200

    Ok chiamata api, inizio grafico

--- js/main.js
+++ js/main.js
@@ -7,20 +7,40 @@ $(document).ready(function () {
         url: 'http://157.230.17.132:4033/sales',
         method: 'GET',
         success: function(data){
-            var sumforMonth = {};
+            var sumforMonth = {
+                'gennaio': 0,
+                'febbraio': 0,
+                'marzo': 0,
+                'aprile': 0,
+                'maggio': 0,
+                'giugno': 0,
+                'luglio': 0,
+                'agosto': 0,
+                'settembre': 0,
+                'ottobre': 0,
+                'novembre': 0,
+                'dicembre': 0
+            };
+            console.log(sumforMonth);
+
             var rispostaTrovaApi = data;
+            console.log(data);
             for (var i = 0; i < rispostaTrovaApi.length; i++) {
                 var dato = rispostaTrovaApi[i]
+                console.log(dato.salesman);
+                console.log(dato.amount);
+                console.log(dato.date);
                 var month = dato.date;
                 var thisMonth = moment(month, 'DD/MM/YYYY').format("MMMM");
                 if (sumforMonth [thisMonth] === undefined) {
                     sumforMonth [thisMonth] = 0;
                 }
                 sumforMonth [thisMonth] += dato.amount;
-
             }
                 var labelsChart = [];
+                console.log(labelsChart);
                 var dataChart = [];
+                console.log(dataChart);
 
                 for (var key in sumforMonth) {
                     labelsChart.push(key);
@@ -34,7 +54,6 @@ $(document).ready(function () {
     });
 
 
-
     // var ctx = $('#grafico');
     // var chart = new Chart(ctx, {
     //
@@ -43,5 +62,6 @@ $(document).ready(function () {
     //     options: options
     //
     // });
+
 
 });
