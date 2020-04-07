db733d5aaafc96ba565d7f6dd6970aa4cbeb4b86
tarq-p <pierluigi.tarquinio90@gmail.com>
2020-04-07 14:59:58 +0200

    inserisco chartjs line

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+// http://157.230.17.132:4033/sales
+
+
+
+
+
+
+
+
+
+var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options
+});
+
+
+});
